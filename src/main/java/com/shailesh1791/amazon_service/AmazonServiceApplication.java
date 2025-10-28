789b3a6dcd56f707550cf8fd53e9c730d525bb68
Shailesh1791 <136346720+Shailesh1791@users.noreply.github.com>
2025-10-28 08:56:46 +0530

    initial commits

---
+++ src/main/java/com/shailesh1791/amazon_service/AmazonServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shailesh1791.amazon_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shailesh1791/amazon_service/AmazonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shailesh1791.amazon_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
